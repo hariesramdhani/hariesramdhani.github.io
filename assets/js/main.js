9e20e555241c3e7ee47dbe979edf50489950489d
hariesramdhani <hydrolizedmaltose@gmail.com>
2018-10-26 03:53:11 +0700

    New look

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function more() {
+    var x = document.getElementById("more");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
