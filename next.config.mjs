89086d613e5eadfdc6c1570fd8c726a45b045fb1
hariesramdhani <hydrolizedmaltose@gmail.com>
2024-10-19 17:18:15 +0100

    Updated next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    images: {
+      loader: "akamai",
+      path: "",
+    },
+    assetPrefix: "./",
+  };
+  
+  export default nextConfig;
