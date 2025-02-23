a43b4d1422bc8b3b0ad4759f3cc5a69fd88c5535
hariesramdhani <hydrolizedmaltose@gmail.com>
2025-02-23 15:05:34 +0000

    Updated photo

---
+++ components/CardGroup.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+    title: "Haries Ramdhani",
+    description:
+        "Github Page of Haries Ramdhani",
+    viewport: "width=device-width, initial-scale=1",
+    icons: {
+        icon: "/img/icon.png",
+    },
+};
+
+export default function Home() {
+    return (
+        <>
+            <p>Just a test</p>
+        </>
+    );
+}
